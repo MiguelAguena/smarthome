6abd610bbb33b2284021c1bdf576831954a7a04f
miguel-usp <miguel.velasques@usp.br>
2023-04-12 20:58:45 -0300

    Clearing added, still has errors

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-var INFLUXDB_TOKEN="JK9gQH30px_47i4uiXKzi1h9mz5cGSQusAb9B1poeP7WSa7ccMXZT7wKS1xemxB6BUaiiSCFUDn29f77C75RPA=="
+// var INFLUXDB_TOKEN="JK9gQH30px_47i4uiXKzi1h9mz5cGSQusAb9B1poeP7WSa7ccMXZT7wKS1xemxB6BUaiiSCFUDn29f77C75RPA=="
+var INFLUXDB_TOKEN = "UzX1k65yoQI5A-ZPUNpXvAkUzweF2YzPJD9ufpI4_E9q06S64v52DNRsT4-kvO3UAMgVPefFCFkTfb0XAKjQ8Q=="
 
 var cors = require('cors')
 var express = require('express');
+const https = require('https')
 
-var {InfluxDB, Point} = require('@influxdata/influxdb-client')
+var { InfluxDB, Point } = require('@influxdata/influxdb-client')
 
 const app = express();
 app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors({
    origin: '*'
 }));
@@ -17,24 +19,50 @@ const url = 'http://localhost:8086'
 const org = `PCS`
 const bucket = `BD`
 
-const client = new InfluxDB({url, token})
+const client = new InfluxDB({ url, token })
 
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
    res.send("Welcome to your SmartHome controller!");
 });
 
-app.post('/writeTemp', function(req, res) {
+app.post('/clearData', function (req, res) {
+   var post_options = {
+      host: 'localhost',
+      port: '8086',
+      path: "/api/v2/delete?org=PCS&bucket=DB",
+      method: 'POST',
+      headers: {
+         'Authorization': 'Token ' + INFLUXDB_TOKEN,
+         'Content-Type': 'application/json'
+      },
+      data: {
+         "start": "2020-01-01T00:00:00Z",
+         "stop": "2024-12-31T00:00:00Z"
+      }
+   };
+
+   // Set up the request
+   var post_req = https.request(post_options, function (dbRes) {
+      dbRes.setEncoding('utf8');
+      dbRes.on('data', function (chunk) {
+         console.log('Response: ' + chunk);
+         res.send(chunk)
+      });
+   });
+});
+
+app.post('/writeTemp', function (req, res) {
    let writeClient = client.getWriteApi(org, bucket, 'ns')
 
    let temp = req.body.temp
    let timestamp = String().concat(req.body.timestamp, "000000")
-   
+
    try {
       let point = new Point('Temperatura')
-                  .floatField('Valor', temp)
-                  .timestamp(timestamp)
+         .floatField('Valor', temp)
+         .timestamp(timestamp)
 
       writeClient.writePoint(point)
 
@@ -43,21 +71,21 @@ app.post('/writeTemp', function(req, res) {
       res.send("SUCCESS");
    }
 
-   catch(err) {
+   catch (err) {
       res.send(err.message);
    }
 });
 
-app.post('/writeHum', function(req, res) {
+app.post('/writeHum', function (req, res) {
    let writeClient = client.getWriteApi(org, bucket, 'ns')
 
    let hum = req.body.hum
    let timestamp = String().concat(req.body.timestamp, "000000")
-   
+
    try {
       let point = new Point('Umidade')
-                  .floatField('Valor', hum)
-                  .timestamp(timestamp)
+         .floatField('Valor', hum)
+         .timestamp(timestamp)
 
       writeClient.writePoint(point)
 
@@ -66,12 +94,12 @@ app.post('/writeHum', function(req, res) {
       res.send("SUCCESS");
    }
 
-   catch(err) {
+   catch (err) {
       res.send(err.message);
    }
 });
 
-app.get('/readTemp', function(req, res) {
+app.get('/readTemp', function (req, res) {
    let queryClient = client.getQueryApi(org)
    fluxQuery = `from(bucket: "` + bucket + `")
       |> range(start: -24h)
@@ -82,23 +110,23 @@ app.get('/readTemp', function(req, res) {
       let queryData = []
       queryClient.queryRows(fluxQuery, {
          next: (row, tableMeta) => {
-           queryData.push(tableMeta.toObject(row))
+            queryData.push(tableMeta.toObject(row))
          },
          error: (error) => {
-           console.error('\nError', error)
+            console.error('\nError', error)
          },
          complete: () => {
-           res.send(queryData);
+            res.send(queryData);
          },
       })
    }
- 
-   catch(err) {
+
+   catch (err) {
       res.send(err.message);
    }
 });
 
-app.get('/readHum', function(req, res) {
+app.get('/readHum', function (req, res) {
    let queryClient = client.getQueryApi(org)
    fluxQuery = `from(bucket: "` + bucket + `")
       |> range(start: -24h)
@@ -109,18 +137,18 @@ app.get('/readHum', function(req, res) {
       let queryData = []
       queryClient.queryRows(fluxQuery, {
          next: (row, tableMeta) => {
-           queryData.push(tableMeta.toObject(row))
+            queryData.push(tableMeta.toObject(row))
          },
          error: (error) => {
-           console.error('\nError', error)
+            console.error('\nError', error)
          },
          complete: () => {
-           res.send(queryData);
+            res.send(queryData);
          },
       })
    }
- 
-   catch(err) {
+
+   catch (err) {
       res.send(err.message);
    }
 });
